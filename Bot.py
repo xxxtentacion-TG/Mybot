0333001d03893c4173ff63497c13c95af334f8ee
xxxᴛᴇɴᴛᴀᴄɪᴏɴ <81632023+xxxtentacion-TG@users.noreply.github.com>
2025-09-19 21:57:32 +0530

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,5 +1,5 @@
 import os
-from pyrogram import Client
+from pyrogram import Client, filters
 
 API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
@@ -7,8 +7,8 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
-@app.on_message()
-def handler(client, message):
-    message.reply_text("Hello, I'm alive!")
+@app.on_message(filters.command("start"))
+def start_command(client, message):
+    message.reply_text("✅ Hello! Your bot is running on Render!")
 
 app.run()
